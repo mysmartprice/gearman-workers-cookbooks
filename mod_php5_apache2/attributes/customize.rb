3d3fbe0f3ff24fbcc1940ace68af6a91415d833a
Arun Chinnachamy <mailarun.bits@gmail.com>
2014-08-29 20:22:56 +0530

    Create customize.rb

---
+++ mod_php5_apache2/attributes/customize.rb
@@ -0,0 +1,22 @@
+normal[:mod_php5_apache2][:packages] = [ 
+    'php5-xsl',
+    'php5-curl',
+    'php5-xmlrpc',
+    'php5-sqlite',
+    'php5-dev',
+    'php5-gd',
+    'php5-cli',
+    'php5-sasl',
+    'php5-mysql',
+    'php5-mcrypt',
+    'php5-memcache',
+    'php-pear',
+    'php-xml-parser',
+    'php-mail-mime',
+    'php-db',
+    'php-mdb2',
+    'php-html-common',
+    'libev-libevent-dev',
+    'uuid-dev',
+    'libgearman-dev'
+]
